6375af9ece2521706d7387f54b7c6df4be32f70e
Alex Karobia <akarobia30@gmail.com>
2024-10-31 04:04:48 +0300

    Week One assigment done

---
+++ WeekOne/user_input.py
@@ -0,0 +1,8 @@
+
+def get_Info():
+  name=input("Enter your name here: ")
+  age=int(input("Enter your age: "))
+  location=input("Enter your Location: ")
+  print("Hello ",name, "You are ",age,"Years old and live in ",location)
+
+get_Info()
