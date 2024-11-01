94d81822a8ee86df469ab2c11b3cd21d40e45a08
Alex Karobia <akarobia30@gmail.com>
2024-11-01 11:45:41 +0300

    Adding Week Two

---
+++ WeekTwo/MyList.py
@@ -0,0 +1,23 @@
+# empty list
+myList=[]
+# append numbers in the list
+myList.append(10)
+myList.append(20)
+myList.append(30)
+myList.append(40)
+
+# inserting a value in the list
+myList.insert(1,15)
+
+# Extending a list in list
+myList.extend([50,60,70])
+
+# Removing Last Item from the list
+myList.pop()
+
+# sorting  myList 
+myList.sort
+# printing the  index of 30
+print(myList.index(30))
+
+print(myList)
